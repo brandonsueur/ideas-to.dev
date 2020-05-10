7dd24f1f1861a491b6beb7c2cb2702013779242b
brandonsueur <gynidark@gmail.com>
2020-05-10 11:03:33 +0200

    fix: error with Axios

--- resources/js/utils/api.js
+++ resources/js/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: window.location.href + "api/v1/", // http://ideas-to.dev.test/api/v1/
+  baseURL: `/api/v1/`,
   responseType: "json"
 });

aad2ab88c2a73864b2b0bd04e499dcf596b5c70c
brandonsueur <gynidark@gmail.com>
2020-05-16 01:38:28 +0200

    feat: create component <Item />

--- public/js/app.js
+++ public/js/app.js
@@ -65237,15 +65237,16 @@ var Header = function Header() {
 
 /***/ }),
 
-/***/ "./resources/js/components/components/Items.js":
-/*!*****************************************************!*\
-  !*** ./resources/js/components/components/Items.js ***!
-  \*****************************************************/
+/***/ "./resources/js/components/components/Item.js":
+/*!****************************************************!*\
+  !*** ./resources/js/components/components/Item.js ***!
+  \****************************************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Item; });
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
@@ -65438,25 +65439,74 @@ function (_Component) {
   return Item;
 }(react__WEBPACK_IMPORTED_MODULE_1__["Component"]);
 
+
+
+/***/ }),
+
+/***/ "./resources/js/components/components/Items.js":
+/*!*****************************************************!*\
+  !*** ./resources/js/components/components/Items.js ***!
+  \*****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _utils_api__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../utils/api */ "./resources/js/utils/api.js");
+/* harmony import */ var _Item__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Item */ "./resources/js/components/components/Item.js");
+/* harmony import */ var _utils_index__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../utils/index */ "./resources/js/utils/index.js");
+
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+
+
+
 var Items =
 /*#__PURE__*/
-function (_Component2) {
-  _inherits(Items, _Component2);
+function (_Component) {
+  _inherits(Items, _Component);
 
   function Items(props) {
-    var _this4;
+    var _this;
 
     _classCallCheck(this, Items);
 
-    _this4 = _possibleConstructorReturn(this, _getPrototypeOf(Items).call(this, props));
-    _this4.state = {
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Items).call(this, props));
+    _this.state = {
       loading: true,
       ideas: null,
       countIdeas: null,
       categories: null,
       categorySelected: null
     };
-    return _this4;
+    return _this;
   }
 
   _createClass(Items, [{
@@ -65470,34 +65520,34 @@ function (_Component2) {
     value: function () {
       var _getCategories = _asyncToGenerator(
       /*#__PURE__*/
-      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee2() {
-        var res, _ref2, data;
+      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+        var res, _ref, data;
 
-        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee2$(_context2) {
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
           while (1) {
-            switch (_context2.prev = _context2.next) {
+            switch (_context.prev = _context.next) {
               case 0:
-                _context2.next = 2;
-                return _utils_api__WEBPACK_IMPORTED_MODULE_3__["default"].get("/categories");
+                _context.next = 2;
+                return _utils_api__WEBPACK_IMPORTED_MODULE_2__["default"].get("/categories");
 
               case 2:
-                res = _context2.sent;
-                _context2.next = 5;
+                res = _context.sent;
+                _context.next = 5;
                 return res.data;
 
               case 5:
-                _ref2 = _context2.sent;
-                data = _ref2.data;
+                _ref = _context.sent;
+                data = _ref.data;
                 this.setState({
                   categories: data
                 });
 
               case 8:
               case "end":
-                return _context2.stop();
+                return _context.stop();
             }
           }
-        }, _callee2, this);
+        }, _callee, this);
       }));
 
       function getCategories() {
@@ -65511,24 +65561,24 @@ function (_Component2) {
     value: function () {
       var _getIdeas = _asyncToGenerator(
       /*#__PURE__*/
-      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee3() {
-        var res, _ref3, data;
+      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee2() {
+        var res, _ref2, data;
 
-        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee3$(_context3) {
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee2$(_context2) {
           while (1) {
-            switch (_context3.prev = _context3.next) {
+            switch (_context2.prev = _context2.next) {
               case 0:
-                _context3.next = 2;
-                return _utils_api__WEBPACK_IMPORTED_MODULE_3__["default"].get("/ideas");
+                _context2.next = 2;
+                return _utils_api__WEBPACK_IMPORTED_MODULE_2__["default"].get("/ideas");
 
               case 2:
-                res = _context3.sent;
-                _context3.next = 5;
+                res = _context2.sent;
+                _context2.next = 5;
                 return res.data;
 
               case 5:
-                _ref3 = _context3.sent;
-                data = _ref3.data;
+                _ref2 = _context2.sent;
+                data = _ref2.data;
                 this.setState({
                   ideas: data,
                   loading: false
@@ -65536,10 +65586,10 @@ function (_Component2) {
 
               case 8:
               case "end":
-                return _context3.stop();
+                return _context2.stop();
             }
           }
-        }, _callee3, this);
+        }, _callee2, this);
       }));
 
       function getIdeas() {
@@ -65561,7 +65611,7 @@ function (_Component2) {
   }, {
     key: "render",
     value: function render() {
-      var _this5 = this;
+      var _this2 = this;
 
       var _this$state = this.state,
           loading = _this$state.loading,
@@ -65571,7 +65621,7 @@ function (_Component2) {
           categorySelected = _this$state.categorySelected;
       return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("h2", {
         className: "text-2xl font-bold"
-      }, "Liste des id\xE9es non exhaustifs"), !loading ? react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", null, countIdeas == null ? ideas.length : countIdeas, " id\xE9es trouv\xE9es") : "", react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("ul", {
+      }, "Liste des id\xE9es non exhaustifs"), !loading && react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", null, countIdeas == null ? ideas.length : countIdeas, " id\xE9es trouv\xE9es"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("ul", {
         className: "list-reset | mt-16 mb-16"
       }, categories && categories.length > 0 && categories.map(function (category) {
         return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("li", {
@@ -65579,7 +65629,7 @@ function (_Component2) {
           className: category.ideas_count > 0 ? "d-inline inline-block" : "hidden"
         }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("a", {
           onClick: function onClick(e) {
-            return _this5.filter(category);
+            return _this2.filter(category);
           },
           className: "block m-1 py-3 px-3 cursor-pointer rounded-lg ".concat(category.name === categorySelected ? "bg-bluedark text-white" : Object(_utils_index__WEBPACK_IMPORTED_MODULE_4__["colorCategory"])(category.name), " font-medium text-base")
         }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("span", {
@@ -65589,15 +65639,13 @@ function (_Component2) {
         }, category.name), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("span", {
           className: "ml-3 text-xs bg-white p-1 ".concat(category.name === categorySelected ? "bg-purple-100 text-bluedark" : "bg-white", " px-3 rounded-full")
         }, category.ideas_count)));
-      })), !loading ? ideas && ideas.length > 0 && ideas.map(function (idea) {
-        return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(Item, {
+      })), !loading && ideas && ideas.length > 0 && ideas.map(function (idea) {
+        return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_Item__WEBPACK_IMPORTED_MODULE_3__["default"], {
           idea: idea,
           key: idea.id,
           visible: idea.category.name === categorySelected || categorySelected == null
         });
-      }) : react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
-        className: "text-center py-8"
-      }, "Chargement ... \uD83D\uDD0E"));
+      }));
     }
   }]);
 
--- resources/js/components/components/Items.js
+++ resources/js/components/components/Items.js
@@ -1,122 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "wouter";
 
 import axios from "../../utils/api";
+import Item from "./Item";
 
 import { colorCategory, emojiCategory } from "../../utils/index";
-import confetti from "canvas-confetti";
-
-class Item extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: null,
-      countLikes: null,
-      isLoading: false
-    };
-
-    this.click = this.click.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState({ isLoading: true });
-
-    const { countLikes } = this.props.idea;
-
-    this.setState({ countLikes });
-  }
-
-  async getCategory() {
-    let res = await axios.get(`ideas/${this.props.params.id}`);
-    let { data } = await res.data;
-
-    this.setState({
-      idea: data,
-      loading: false
-    });
-  }
-
-  click(e) {
-    e.preventDefault();
-
-    const { id } = this.props.idea;
-
-    axios
-      .post("/likes", { idea_id: id })
-      .then(response => {
-        if (response.status === 401) {
-          return this.setState({
-            countLikes: response.data.countLikes
-          });
-        } else {
-          confetti({
-            particleCount: 60,
-            spread: 100,
-            origin: { y: 1.2 }
-          });
-
-          this.setState({
-            countLikes: response.data.countLikes,
-            data: response.data,
-            isLoading: false
-          });
-        }
-      })
-      .catch(error => console.log(error.response));
-  }
-
-  render() {
-    const { countLikes } = this.state;
-    const { idea, visible } = this.props;
-
-    return (
-      <div
-        data-id={idea.id}
-        data-category={idea.category.name}
-        className={`flex container h-32 rounded-lg my-8 ${colorCategory(
-          idea.category.name
-        )} ${!visible && "hidden"}`}
-      >
-        <div className="w-5/6 flex">
-          <div className="w-1/6 justify-center text-center flex self-center">
-            <a className="text-3xl" href="#">
-              {emojiCategory(idea.category.name)}
-            </a>
-          </div>
-
-          <div className="w-5/6 self-center">
-            <Link href={`/ideas/${idea.id}`}>
-              <h1 className="font-semibold text-2xl mb-2 cursor-pointer">
-                {idea.title}
-              </h1>
-            </Link>
-
-            <p className="leading-none">
-              <strong className="count-like">
-                {!this.state.isLoading ? countLikes : idea.likes_count}
-              </strong>{" "}
-              personnes ont aimé cette idée !
-            </p>
-          </div>
-        </div>
-
-        <div className="w-1/6 flex">
-          <div className="justify-center text-center flex self-center w-full">
-            <a
-              onClick={e => this.click(e)}
-              disabled={this.state.isLoading}
-              className="px-5 py-4 bg-white rounded-lg text-2xl shadow-lg hover:shadow-xl like"
-              href="#"
-            >
-              👌
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
 class Items extends Component {
   constructor(props) {
@@ -174,10 +61,9 @@ class Items extends Component {
     return (
       <div>
         <h2 className="text-2xl font-bold">Liste des idées non exhaustifs</h2>
-        {!loading ? (
+
+        {!loading && (
           <p>{countIdeas == null ? ideas.length : countIdeas} idées trouvées</p>
-        ) : (
-          ""
         )}
 
         <ul className="list-reset | mt-16 mb-16">
@@ -222,7 +108,7 @@ class Items extends Component {
             })}
         </ul>
 
-        {!loading ? (
+        {!loading &&
           ideas &&
           ideas.length > 0 &&
           ideas.map(idea => {
@@ -236,10 +122,7 @@ class Items extends Component {
                 }
               />
             );
-          })
-        ) : (
-          <p className="text-center py-8">Chargement ... 🔎</p>
-        )}
+          })}
       </div>
     );
   }

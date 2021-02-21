ca0864f12cc9e366ef6157a291cef1c1eafdd941
Brandon Sueur <brandon.sueur@outlook.fr>
2021-02-22 00:18:05 +0100

    chore: remove laravel

--- resources/js/components/pages/Ideas.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-import Items from "../components/Items";
-
-export const PageIdeas = () => <Items />;
--- resources/js/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { PageHome } from "./Home";
-export { PageAbout } from "./About";
-export { PageIdea } from "./Idea";
-export { PageIdeas } from "./Ideas";
-export { PageAdd } from "./Add";
-export { PageRoadmap } from "./Roadmap";
-export { PageError } from "./Error";
